2b67e110591cf1a932f27c7a4cce8edba38e4acd
Nick Romans <nicholas.romans@gmail.com>
2018-08-31 00:26:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.include?("cheddar", "gouda", "camembert")
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include?(cheese_types)
 end
-#cheese_types = ["cheddar", "gouda", "camembert"]
-#end
+
